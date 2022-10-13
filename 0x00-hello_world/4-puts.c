025af624c066361ca6beec5ec9d027608adb1495
Nat30b <natnaelbirhan11@gmail.com>
2022-10-13 12:37:42 -0700

    A program that prints string by using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 /*
  *A program that prints
  *a string of characters
+ *main - main entry
+ *Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
