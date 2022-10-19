f52e63366ee0ce7ed1b376c2afdce8b9b413da9c
Nat30b <natnaelbirhan11@gmail.com>
2022-10-19 09:35:00 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *Jack Bauer : Starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	for (a == 00; a < 24; a++)
+	{
+		for (b == 00; b <= 59; b++)
+			printf("%d : %d", a, b);
+	}
+}
