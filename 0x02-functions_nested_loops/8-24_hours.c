6622cc03292efd074b0f4f111888ec6c5c0364b8
Nat30b <natnaelbirhan11@gmail.com>
2022-10-19 09:52:23 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *jack_bauer :> Starting from 24
+ * jack_bauer -> prints 24 hours
  */
 void jack_bauer(void)
 {
-	int a;
-	int b;
+int i, j;
 
-	for (a = 0; a < 24; a++)
-	{
-		for (b = 0; b <= 59; b++)
-			printf("%d : %d\n", a, b);
-	}
+for (i = 0; i < 24; i++)
+{
+for (j = 0; j < 60; j++)
+{
+if (i < 10)
+{
+_putchar('0');
+_putchar(i + '0');
+}
+else if (i >= 10)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+if (j < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(j + '0');
+}
+else if (j >= 10)
+{
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
+
+}
 }
