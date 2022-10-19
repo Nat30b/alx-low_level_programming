cd6940c40cddc52a6955fb89b243ee94293effcf
Nat30b <natnaelbirhan11@gmail.com>
2022-10-19 00:46:19 -0700

    Task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
-include "main.h"
+#include "main.h"
 /**
  *main - main entry
  *A code that prints a lower case alphabet
  */
 int main(void)
 {
-	int i;
-	for(i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-	return (0);
-
+int i;
+for(i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
