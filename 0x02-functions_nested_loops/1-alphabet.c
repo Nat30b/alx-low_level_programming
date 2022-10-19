99215286f03ec8ae35d7f9dbe65f51846c32421b
Nat30b <natnaelbirhan11@gmail.com>
2022-10-19 00:37:42 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+include <main.h>
+/**
+ *main - main entry
+ *A code that prints a lower case alphabet
+ */
+int main(void)
+{
+	int i;
+	for(i='a', i<='z', i++)
+	{
+		_putchar(i);
+	}
+	_putchar(\n);
+	return (0);
+
+}
