b31ab7a5cdbe1fea6d29a77a8e43a9ab3f7dd034
Nat30b <natnaelbirhan11@gmail.com>
2022-10-20 01:44:15 -0700

    Prints first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *fibonacci :> first 50 fibonacci numbers
+ */
+int main(void)
+{
+	int i, a, b, result;
+	a=0;
+	b=1;
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d\n, ", a);
+		result = a + b;
+		a = b;
+		b = result;
+	}
+}
+
